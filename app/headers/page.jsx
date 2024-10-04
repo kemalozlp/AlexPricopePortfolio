1e94189e2b8f6a1a0049a212b7a178c3ad4784fa
kemalozlp <ozlpkemal@gmail.com>
2024-10-04 18:37:06 +0300

    Headers Tamamland ı import edildi

--- app/headers/page.jsx
+++ app/headers/page.jsx
@@ -1,2 +1,43 @@
-import styles from "./page.module.css";
+import Link from "next/link";
+import "../globals.css";
 
+export default function Headers() {
+  return (
+    <div className="Headers">
+      <h3>Alex Pricope</h3>
+      <div className="headersurls">
+        <Link href="">
+          Home
+        </Link>
+
+        <Link href="">
+          About
+        </Link>
+
+        <Link href="">
+          Achievement
+        </Link>
+
+        <Link href="">
+          Our Skills
+        </Link>
+
+        <Link href="">
+          Services
+        </Link>
+
+        <Link href="">
+          Portfolio
+        </Link>
+
+        <Link href="">
+          Contact
+        </Link>
+      </div>
+
+      <button className="headerBtn" >View CV</button>
+
+    </div>
+  )
+
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Headers from "./headers/page";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,7 +23,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+       <div className="container">
+       <Headers />
+       {children}
+       </div>
       </body>
     </html>
   );
