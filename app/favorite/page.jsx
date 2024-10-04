c2ec4ce8e3e0f48935c71580a4caed004dd15dcb
kemalozlp <ozlpkemal@gmail.com>
2024-10-04 23:01:46 +0300

    Favorite componenti tasarımı ile birlikte bitti

--- app/favorite/page.jsx
+++ app/favorite/page.jsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
 import "../globals.css";
+import Image from "next/image";
 
 
-export default function Favorite () {
+export default function Favorite() {
   return (
-   <div className="favorite">
-    
-   </div>
+    <div className="favorite">
+      <div className="favoriteText">
+        <h3>Achievement</h3>
+        <h1>My favorite Projects</h1>
+        <p>Explore my favorite projects that showcase my skills and creativity. Each project presents a unique challenge and demonstrates my commitment to quality and innovation. Discover what inspires me and how I approach design and development!</p>
+        <Link href="/">Explore More <span>→</span></Link>
+      </div>
+      <div className="favoriteBox">
+        <Link href="/">xxxxxxxxxxxxxx <Image width={30} height={30} src="/images/arrowup.svg" /></Link>
+        <Link href="/">xxxxxxxxxxxxxx <Image width={30} height={30} src="/images/arrowup.svg" /></Link>
+        <Link href="/">xxxxxxxxxxxxxx <Image width={30} height={30} src="/images/arrowup.svg" /></Link>
+      </div>
+
+    </div>
   )
 }
